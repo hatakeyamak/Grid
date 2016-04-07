e5bb820f6e357220ce247b467af12a5be70b0dc6
Kenichi Hatakeyama <kenichi.hatakeyama@cern.ch>
2016-04-07 07:57:34 -0500

    crab3 test

---
+++ CrabTestPackage/crab3Config_WE_7TeV.py
@@ -0,0 +1,51 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+
+config.section_("General")
+config.General.requestName = ''
+
+config.section_("JobType")
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'WE_7TeV_cfi_GEN_SIM_DIGI_L1_DIGI2RAW_RAW2DIGI_RECO.py'
+#config.JobType.allowNonProductionCMSSW = False 
+config.JobType.allowUndistributedCMSSW = False # Parameter JobType.allowNonProductionCMSSW has been renamed to JobType.allowUndistributedCMSSW
+
+config.section_("Data")
+#config.Data.inputDataset = '/SingleMuon/Run2015B-PromptReco-v1/MINIAOD'
+#config.Data.inputDataset = '/SingleMuon/Run2015B-17Jul2015-v1/MINIAOD'
+#config.Data.inputDataset = '/HTMHT/Run2015B-PromptReco-v1/MINIAOD'
+#config.Data.inputDataset = '/HTMHT/Run2015B-17Jul2015-v1/MINIAOD'
+
+#config.Data.inputDBS = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'  # Parameter Data.dbsUrl has been renamed to Data.inputDBS
+#config.Data.dbsUrl = 'https://cmsweb.cern.ch/dbs/prod/global/DBSReader/'
+#config.Data.dbsUrl = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSReader/'
+#config.Data.splitting = 'FileBased'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 10
+NJOBS = 30
+config.Data.totalUnits = config.Data.unitsPerJob * NJOBS
+#config.Data.inputDBS = 'global'
+#config.Data.inputDBS = 'global'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/DCSOnly/json_DCSONLY_Run2015B.txt'
+#config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions15/13TeV/Cert_246908-251252_13TeV_PromptReco_Collisions15_JSON.txt'
+#config.Data.lumiMask = './json_Run2015_DCSOnly_251244to251721_17July2015.txt'
+#config.Data.runRange = '251244-251721'
+#config.Data.lumiMask = './json_DCSONLY_Run2015B_ammended_720.txt'
+#config.Data.runRange = '251244-251883'
+#config.Data.lumiMask = './Cert_246908-251883_13TeV_PromptReco_Collisions15_JSON_v2.txt'
+#config.Data.runRange = '251563-251883'
+#config.Data.publication = False
+#config.Data.publishDBS = 'https://cmsweb.cern.ch/dbs/prod/phys03/DBSWriter/' # Parameter Data.publishDbsUrl has been renamed to Data.publishDBS
+#config.Data.publishDataName = 'Run2015B_HTMHT_DCSjson_v03'
+
+config.Data.outLFNDirBase = '/store/user/hatake/ntuples/test'  # Data.outLFN has been renamed to Data.outLFNDirBase
+config.Data.outputPrimaryDataset = 'WE_7TeV'
+config.Data.ignoreLocality = False
+
+config.section_("Site")
+config.Site.storageSite = 'T3_US_Baylor'
+#KH (this whitelisting below is not really necessary. we can use any T2/T3 for running jobs. we can still send output to Baylor)
+config.Site.whitelist = ['T3_US_Baylor']
+
+config.General.transferLogs=True 
+
